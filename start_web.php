667aea7944afd433acdd43f899c5242247b03505
wangwen <8863704@qq.com>
2018-01-13 00:29:54 +0800

    lll

--- start_web.php
+++ start_web.php
@@ -19,9 +19,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 // WebServer
 $web = new WebServer("http://0.0.0.0:8088");
 // WebServer数量
-$web->count = 2;
+$web->count = 1;
 // 设置站点根目录
-$web->addRoot('www.your_domain.com', __DIR__.'/Web');
+$web->addRoot('ximo.me', __DIR__.'/Web');
 
 
 // 如果不是在根目录启动，则运行runAll方法
